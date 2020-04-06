34b2fce30a0073853d1ba6cefc3c09c25af09302
Martin Doms <iamaelephant@gmail.com>
2020-04-06 15:48:40 +1200

    Zoomable

--- components/map.js
+++ components/map.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { scaleLinear } from "d3-scale";
 
-import { ComposableMap, Geographies, Geography } from "react-simple-maps";
+import { ComposableMap, Geographies, Geography, ZoomableGroup } from "react-simple-maps";
 
 
 function Map({ data, min, max, property }) {
@@ -12,30 +12,26 @@ function Map({ data, min, max, property }) {
         .domain([min, max])
         .range(["#D9D9D9", "#AA0011"]);
 
-    console.log(data.map(p => p.Country));
     return (
         <div>
             <ComposableMap>
-                <Geographies geography={geoUrl}>
-                    {({ geographies }) =>
-                        geographies.map(geo => {
-                            var geoData = data.find(p => p.Country == geo.properties.NAME || p.Country == geo.properties.NAME_LONG || p.Country == geo.properties.ISO_A3 || p.Country == geo.properties.ISO_A2);
+                <ZoomableGroup zoom={1}>
+                    <Geographies geography={geoUrl}>
+                        {({ geographies }) =>
+                            geographies.map(geo => {
+                                var geoData = data.find(p => p.Country == geo.properties.NAME || p.Country == geo.properties.NAME_LONG || p.Country == geo.properties.ISO_A3 || p.Country == geo.properties.ISO_A2);
 
-                            if (!geoData) {
-                                console.log(`Could not map geo ${geo.properties.NAME}`);
-                                console.log(geo);
-                            }
-
-                            if (geoData && !geoData[property]) geoData = null;
-                            
-                            return <Geography 
-                                key={geo.rsmKey} 
-                                geography={geo} 
-                                fill={geoData ? colorScale(geoData[property]) : "#EEE"}
-                            />
-                        })
-                    }
-                </Geographies>
+                                if (geoData && !geoData[property]) geoData = null;
+                                
+                                return <Geography 
+                                    key={geo.rsmKey} 
+                                    geography={geo} 
+                                    fill={geoData ? colorScale(geoData[property]) : "#EEE"}
+                                />
+                            })
+                        }
+                    </Geographies>
+                </ZoomableGroup>
             </ComposableMap>
         </div>
     )
