35418996859ccd73c21a747d10fed8ee9046363d
Martin Doms <iamaelephant@gmail.com>
2020-04-16 11:51:20 +1200

    Added top 5 table by metric

---
+++ src/website/components/table.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+
+export function CountryTable({ data, metric }) {
+    var sorted = data.sort((first, second) => metric.calc(second) - metric.calc(first));
+    return (
+        <TableContainer>
+            <Table>
+                <TableHead>
+                    <TableCell width={'30%'}>Country</TableCell>
+                    <TableCell>{metric.name}</TableCell>
+                </TableHead>
+                <TableBody>
+                    {
+                        sorted.slice(0, 5).map((row) =>
+                            <TableRow>
+                                <TableCell>{row.Country}</TableCell>
+                                <TableCell>{metric.calc(row)}</TableCell>
+                            </TableRow>
+                        )
+                    }
+                </TableBody>
+                
+            </Table>
+        </TableContainer>
+    );
+}
